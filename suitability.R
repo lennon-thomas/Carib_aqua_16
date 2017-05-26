aa40f6cc1aeed046781ba45a7f527554ee3299bc
lennon-thomas <lennon.rose.thomas@gmail.com>
2017-05-26 13:09:15 -0700

    suitability results added

--- suitability.R
+++ suitability.R
@@ -10,6 +10,8 @@
 rm(list = ls())
 library(raster)
 library(rgdal)
+library(tmap)
+library(dplyr)
 
 boxdir<-('/Users/lennonthomas/Box Sync/Waitt Institute/Blue Halo 2016/Carib_aqua_16/')
 
@@ -62,13 +64,12 @@ writeRaster(filename=paste(boxdir,"Suitability/final/suitable_depth.tif",sep="")
 
 MPA<-raster(paste(boxdir,'Suitability/tmp/final_mpa_layer.tif',sep=""))
 
-MPA<-mask(MPA, raster_eez, maskvalue = 1)
 
 png(file=paste(boxdir,"Suitability/plots/suitable_mpa.png",sep=""))
 
 plottitle<-("MPA areas")
 
-plot(MPA,main=plottitle,col="red")
+plot(MPA,main=plottitle)
 
 dev.off()
 
@@ -143,8 +144,6 @@ oil<-raster(paste(boxdir,"Suitability/final/suitable_oil.tif",sep=""))
 
 oil[oil>0]<-0
 
-
-
 png(file=paste(boxdir,"Suitability/plots/suitable_oil.png",sep=""))
 
 plottitle<-("Oil Structure")
@@ -173,15 +172,11 @@ s = stack(carib_depth,MPA,current,ships,raster_eez,oil,coral)
 
 s<-stackApply(s,indices=c(1,1,1,1,1,1,1),fun=prod,na.rm=TRUE)
 
-all[all==0]<-NA
-
-all<-mask(s,raster_eez,maskvalues=NA,inverse=FALSE,filename=paste(boxdir,"Suitability/results/final_suitable_areas.tif",sep=""),overwrite=TRUE)
+all_suitable<-mask(s,raster_eez,maskvalues=NA,inverse=FALSE,filename=paste(boxdir,"Suitability/results/final_suitable_areas.tif",sep=""),overwrite=TRUE)
 
 saveRDS(all,paste(boxdir,"Suitability/final/suitable_areas.rds",sep=""))
 
-suitable_area<-as.data.frame(area(all,na.rm=TRUE))
-
-final_suitable_area<-sum(suitable_area,na.rm=TRUE)
+all_suitable[all_suitable == 0]<-NA
 
 #-----------------------------------------------------------------
 #----------------------------------------------------------------
@@ -191,7 +186,7 @@ study_area<-as.data.frame(area(raster_eez,na.rm = TRUE))
 
 study_area<-sum(study_area,na.rm = TRUE)
 
-suitable_areas<-as.data.frame(area(all,na.rm = TRUE))
+suitable_areas<-as.data.frame(area(all_suitable,na.rm = TRUE))
 
 total_suitable_area<-sum(suitable_areas,na.rm = TRUE)
 
@@ -205,48 +200,138 @@ EEZ = readOGR(dsn=paste(boxdir,"Suitability/tmp",sep = ""),layer="carib_eez_shap
 
 country<-raster(paste(boxdir,"Suitability/tmp/carib_eez_raster.tif",sep = ""))
 
-names<-data.frame(matrix(NA,nrow=33,ncol=2))
-
 EEZ<-as.data.frame(EEZ)
 
-c<-EEZ[,2:3]
+names<-as.character(EEZ$GeoName)
 
-cell_area<-area(all,na.rm=TRUE)
+c_area<-EEZ$Area_km2
 
-cells<-zonal(all,EEZ,fun='sum')
+country_area<-area(all_suitable,na.rm=TRUE)
 
-cells<-cells[-1,]
+country_suitable<-zonal(country_area,country,fun='sum')
 
-for (i in 1:nrow(cells)){
-m<-as.integer(cells[i,1])
-names[i,1]<-m
-n<-c$rgn_name[c$rgn_id==m]
-n<-as.character(n[1])
-names[i,2]<-n
-}
+country_suitable<-as.data.frame(country_suitable)
 
-cells<-cbind(cells,names)
-cells<-cells[,-3]
-cells[order(cells$sum,decreasing=TRUE),]
-write.csv(cells,"final/suitable area by country.csv")
-# 
+percent_eez_suitable = country_suitable$sum/c_area*100
 
-# ####tmaps######
-# test<-select(country)
-# library(tmap)
-# data(World)
-# 
-# land = readOGR(dsn="C:/Users/Lennon Thomas/Desktop/Carib_aqua_16/Suitability/tmp/EEZ",layer="carib_eez_shape")
-# land$la<-ifelse(land@data$Pol_type=="200NM","blue","orange")
-# 
-# suitable<-tm_shape(land)+
-#   tm_fill("la")+
-# tm_shape(all)+
-#   tm_raster(showNA=FALSE,legend.show=FALSE)+
-# tm_shape(land)+
-#   tm_borders()
-# 
-# save_tmap(suitable,filename="datalayer plots/suitability.png")
-# tm.layout(legend.position=c("right","top"))
-# 
-# 
+country_suitable<-cbind(names,c_area,country_suitable,percent_eez_suitable) 
+
+
+
+country_suitable<-country_suitable[,-3]
+
+country_suitable<- country_suitable[order(-percent_eez_suitable),]
+
+colnames(country_suitable)<-c("EEZ","Total Area","Suitable Area","Percent Suitable")
+
+country_suitable$EEZ<-as.character(country_suitable$EEZ)
+
+write.csv(country_suitable,paste(boxdir,filename = "Suitability/results/suitable area by eez.csv",sep = ""))
+
+
+#-----------------------------------------------------------------
+#----------------------------------------------------------------
+# Determine total area (km2) of area that was identified as suitable
+
+data(World)
+
+EEZ = readOGR(dsn=paste(boxdir,"Suitability/tmp",sep = ""),layer="carib_eez_shape")
+
+suitable<-raster(paste(boxdir,"Suitability/results/final_suitable_areas.tif",sep = ""))
+
+great_ant = c("Joint regime area Honduras / Cayman Islands",
+                    "Cuban Exclusive Economic Zone","Cayman Islands Exclusive Economic Zone",
+                    "Jamaican Exclusive Economic Zone",
+                    "Haitian Exclusive Economic Zone",
+                    "Joint regime area Colombia / Dominican Republic",
+                    "Joint regime area Colombia / Jamaica",
+                     "Dominican Republic Exclusive Economic Zone")
+
+luc_arc<-c("Turks and Caicos Exclusive Economic Zone", "Bahamas Exclusive Economic Zone")
+
+lee_ant <-c("Aruban Exclusive Economic Zone","Bonaire Exclusive Economic Zone","Curaçaoan Exclusive Economic Zone")
+
+EEZ@data$region<-as.factor(ifelse(EEZ@data$GeoName %in% great_ant, "Greater Antilles",
+                                  ifelse(EEZ@data$GeoName %in% luc_arc, "Lucayan Archipelago", 
+                                         ifelse(EEZ@data$GeoName %in% lee_ant, "Leeward Antilles","Lesser Antilles"))))
+
+GA<- EEZ[EEZ$GeoName %in% great_ant,]
+
+GA_suit<-mask(suitable,GA)
+
+GA_suit<-crop(GA_suit,GA)
+
+
+
+suitable<-raster(paste(boxdir,"Suitability/results/final_suitable_areas.tif",sep = ""))
+
+LUC<-EEZ[EEZ@data$region == "Lucayan Archipelago",]
+
+LUC_suit<-mask(suitable,LUC)
+
+LUC_suit<-crop(LUC_suit,LUC)
+
+
+
+suitable<-raster(paste(boxdir,"Suitability/results/final_suitable_areas.tif",sep = ""))
+
+LEE<- EEZ[EEZ$GeoName %in% lee_ant,]
+
+LEE_suit<-mask(suitable,LEE)
+
+LEE_suit<-crop(LEE_suit,LEE)
+
+
+suitable<-raster(paste(boxdir,"Suitability/results/final_suitable_areas.tif",sep = ""))
+
+LS<- EEZ[EEZ@data$region == "Lesser Antilles",]
+
+LS_suit<-mask(suitable,LS)
+
+LS_suit<-crop(LS_suit,LS)
+
+
+
+
+
+g_antilles_fig<-  tm_shape(GA_suit) +
+                    tm_raster(showNA = FALSE, legend.show = FALSE, palette = c("white","red"),labels = c("","Suitable Areas"), title = "Greater Antilles") +
+                  tm_shape(GA,is.master = TRUE) +
+                    tm_fill(col="lightblue",alpha = 0.4,title = "Greater Antilles") +
+                    tm_borders(lwd = 1.2) +
+                    tm_legend(main.title.size = 2, main.title="Greater Antilles", position = c("right","top"))
+                   
+                 
+                  
+
+luc_arc_suitable.fig<- tm_shape(LUC_suit) +
+                          tm_raster(showNA = FALSE, legend.show = FALSE, palette = c("white","red"),labels = c("","Suitable Areas")) +
+                       tm_shape(LUC,is.master = TRUE) +
+                          tm_fill(col="lightblue",alpha = 0.4,title ="TESt") +
+                          tm_borders(lwd = 1.2) +
+                          tm_legend(main.title.size = 2, text.size = 1, position = c("right","top"),main.title = "Lucayan Archipelago")
+
+
+
+lee_ant_suitable.fig<- tm_shape(LEE_suit) +
+                          tm_raster(showNA = FALSE, legend.show = FALSE, palette = c("white","red"),labels = c("","Suitable Areas")) +
+                       tm_shape(LEE,is.master = TRUE) +
+                          tm_fill(col="lightblue",alpha = 0.4,title = "Leeward Antilles") +
+                          tm_borders(lwd = 1.2) +
+                          tm_legend(main.title.size = 2, text.size = 1, position = c("right","top"), main.title = "Leeward Antilles")
+
+
+less_ant_suitable.fig<- tm_shape(LS_suit) +
+                          tm_raster(showNA = FALSE, legend.show = FALSE, palette = c("white","red"),labels = c("","Suitable Areas")) +
+                       tm_shape(LS,is.master = TRUE) +
+                          tm_fill(col="lightblue",alpha = 0.4,title = "Leeward Antilles") +
+                          tm_borders(lwd = 1.2) +
+                          tm_legend(main.title.size = 2, text.size = 1, position = c("right","top"), main.title = "Lesser Antilles")
+
+
+all_plots<- tmap_arrange(g_antilles_fig, luc_arc_suitable.fig,lee_ant_suitable.fig,less_ant_suitable.fig)
+
+save_tmap(all_plots,filename = paste(boxdir,"Suitability/results/suitability_figure_zoom.png",sep = ""))
+
+
+save.image(file = paste(boxdir,'Suitability/results/suitability_results.Rdata',sep=""))
