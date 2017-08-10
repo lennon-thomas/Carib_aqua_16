178f369ee74e859a2dd30ed9cf13fdd28680c560
lennon-thomas <lennon.rose.thomas@gmail.com>
2017-08-10 12:13:32 -0700

    whoops

---
+++ code scraps.R
@@ -0,0 +1,54 @@
+## Some examples of how to work with raster stacks
+
+econ_stack[["fuel_price"]][1:100] # example of how to subset cells of layer 
+
+m1<-cellStats(prod[[1]],"max") #apply a function to a layer
+
+test<-calc(prod,function(x)x *depth)   #econ_stack[["permit_fee"]]) ## apply functions to an entire raster stack
+
+# Demo function for raster math
+test_function <- function(rast_1, rast_2) {
+  old_values1 <- getValues(rast_1) # get values for first raster
+  old_values2 <- getValues(rast_2) # get values for second raster
+  new_values <- old_values1 * old_values2 # do raster math
+  new_rast <- setValues(rast_1, new_values) # update values for first raster and return as new raster. !! Make sure this is not overwriting values of rast_1 file but actually creating a new raster
+  return(new_rast)
+}
+
+test <- test_function(rast_1 = prod[[1]], rast_2 = fuel_price)
+
+m2<-cellStats(test[[1]],"max") #compare with m1 (should be x100 greater)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## Wave exposure layer (not really sure if/how to incorporate this layer yet for increased mainentance costs on exposed areas)
+
+waves<-raster(paste(boxdir,"economic/data/PECS/PECS_exposure.tif",sep=""))
+hurricanes<-raster(paste(boxdir,"economic/data/PECS/PECS_hurricanes.tif",sep=""))
+carib_waves<-crop(waves,depth)
+carib_waves<-resample(waves,depth)
+carib_waves<-mask(carib_waves,shore_distance)
+
+
+normal_waves<-10^log_waves
+
+# Test by multiplying permit values times production quantities (meaningless, can delete)
+test2 <- prod_values * permit_values
+
+
+
+
+
+
